99608725935b655dd567d1e4b873f2f7f1d4357f
yuvraj jaiswal <yuvrajjaiswal655@gmail.com>
2021-12-16 14:52:58 +0530

    First Commit

---
+++ CartPole-Deep-Q-learning-Training.py
@@ -0,0 +1,36 @@
+import gym
+
+env = gym.make('CartPole-v1')
+states = env.observation_space.shape[0]
+actions = env.action_space.n
+
+##
+
+from rl.agents import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import Sequential
+
+model = Sequential()
+model.add(Flatten(input_shape=(1,states)))
+model.add(Dense(24,activation="relu"))
+model.add(Dense(24,activation="relu"))
+model.add(Dense(actions,activation="linear"))
+
+policy = BoltzmannQPolicy()
+memory = SequentialMemory(limit=50000,window_length=1)
+agent = DQNAgent(model,policy,memory=memory,nb_actions=actions,nb_steps_warmup=10,target_model_update=1e-2)
+
+agent.compile(Adam(),metrics=["mae"])
+agent.fit(env,nb_steps=20000,visualize=False)
+
+##
+import numpy as np
+Test_agent = agent.test(env,nb_episodes=5,visualize=True)
+print(np.mean(Test_agent.history['episode_reward']))
+env.close()
+
+##
+agent.save_weights(filepath='AgentWeights')
